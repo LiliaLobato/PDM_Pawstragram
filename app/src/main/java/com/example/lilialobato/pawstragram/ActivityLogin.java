bdacdffb9f3fda05dbedf6ed3720a9b0222cb2f5
Carlo Bruno <carlobruno_fb@hotmail.com>
2019-04-01 04:34:11 -0600

    removed fb logo

--- app/src/main/java/com/example/lilialobato/pawstragram/ActivityLogin.java
+++ app/src/main/java/com/example/lilialobato/pawstragram/ActivityLogin.java
@@ -1,13 +1,21 @@
 package com.example.lilialobato.pawstragram;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class ActivityLogin extends AppCompatActivity {
+public class ActivityLogin extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void login(View v){
+        Intent intent = new Intent(this, MainMenu.class);
+        this.startActivity(intent);
     }
 }
