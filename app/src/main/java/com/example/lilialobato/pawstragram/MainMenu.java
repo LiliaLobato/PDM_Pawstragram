14ef3877c2d4d60cec39e8333571bc007b149334
Carlo Bruno <carlobruno_fb@hotmail.com>
2019-05-01 19:57:11 -0500

    added new views

--- app/src/main/java/com/example/lilialobato/pawstragram/MainMenu.java
+++ app/src/main/java/com/example/lilialobato/pawstragram/MainMenu.java
@@ -14,15 +14,21 @@ import android.widget.LinearLayout;
 
 
 import com.example.lilialobato.pawstragram.fragments.FragmentHome;
+import com.example.lilialobato.pawstragram.fragments.FragmentProfile;
 import com.example.lilialobato.pawstragram.fragments.FragmentSearch;
 
 public class MainMenu extends Activity {
 
 
     FragmentHome fragmentHome;
+    FragmentProfile fragmentProfile;
+    FragmentSearch fragmentSearch;
+
     LinearLayout container;
 
     FragmentManager fragmentManager;
+
+    boolean exit = false;
 
 
     @Override
@@ -35,27 +41,51 @@ public class MainMenu extends Activity {
         fragmentTransaction.replace(R.id.container, new FragmentHome());
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
+        exit = true;
 
     }
 
-    public void openMenu(View v) {
+    @Override
+    public void onBackPressed() {
+        if(exit){
+            Intent homeIntent = new Intent(Intent.ACTION_MAIN);
+            homeIntent.addCategory( Intent.CATEGORY_HOME );
+            homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(homeIntent);
+        }
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.container, new FragmentHome());
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
+        exit = true;
+    }
+
+    public void openMenu(View v) {
+        exit = true;
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container, new FragmentHome());
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
     }
 
     public void goPerfil(View v) {
-        Intent intent = new Intent(this, ActivityUserProfile.class);
-        this.startActivity(intent);
+        exit = false;
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container, new FragmentProfile());
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
 
     }
 
     public void openSearch(View v){
+        exit = false;
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.container, new FragmentSearch());
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
+
     }
 
 
---
+++ app/src/main/java/com/example/lilialobato/pawstragram/fragments/FragmentProfile.java
@@ -0,0 +1,21 @@
+package com.example.lilialobato.pawstragram.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.lilialobato.pawstragram.R;
+
+public class FragmentProfile extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+
+
+        return rootView;
+    }
+}
