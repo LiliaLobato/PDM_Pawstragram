c0fcd42c5434b1c3dbf588dfe8d5e6ea8336c593
LiliaLobato <ie706937@iteso.mx>
2019-05-02 00:53:21 -0500

    Sign up & sign in

--- app/src/main/java/com/example/lilialobato/pawstragram/ActivityLogin.java
+++ app/src/main/java/com/example/lilialobato/pawstragram/ActivityLogin.java
@@ -1,76 +1,93 @@
 package com.example.lilialobato.pawstragram;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-
-import com.facebook.AccessToken;
-import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.login.LoginManager;
-import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
-
-import java.util.Arrays;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class ActivityLogin extends Activity {
-    private static final String EMAIL = "email";
-    LoginButton loginButton;
-    CallbackManager callbackManager;
 
+    LinearLayout mLoginContainer;
+    AnimationDrawable mAnimationDrawable;
+
+    EditText username_et,password_et;
+    ProgressDialog mProgrssDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        loginButton = findViewById(R.id.login_button);
-        loginButton.setReadPermissions(Arrays.asList(EMAIL));
 
-        callbackManager = CallbackManager.Factory.create();
-        Toast.makeText(this,"token = " + AccessToken.getCurrentAccessToken(), Toast.LENGTH_LONG).show();
+        //Animation config
+        mLoginContainer = (LinearLayout) findViewById(R.id.login_container);
+        mAnimationDrawable = (AnimationDrawable) mLoginContainer.getBackground();
+        mAnimationDrawable.setEnterFadeDuration(5000);
+        mAnimationDrawable.setExitFadeDuration(2000);
 
-        LoginManager.getInstance().registerCallback(callbackManager,
-                new FacebookCallback<LoginResult>() {
-                    @Override
-                    public void onSuccess(LoginResult loginResult) {
-                        // App code
-                        Log.e("login", "login success");
-                    }
-
-                    @Override
-                    public void onCancel() {
-                        // App code
-                        Log.e("login", "login cancel");
-                    }
-
-                    @Override
-                    public void onError(FacebookException exception) {
-                        // App code
-                        Log.e("login", "login error");
-                    }
-
-                });
+        username_et = (EditText) findViewById(R.id.user_name);
+        password_et = (EditText) findViewById(R.id.user_password);
+        mProgrssDialog = new ProgressDialog(this);
     }
 
     public void login(View v){
-        Intent intent = new Intent(this, MainMenu.class);
-        this.startActivity(intent);
+        logIn();
     }
     public void signup(View v){
         Intent intent = new Intent(this, ActivitySignup.class);
         this.startActivity(intent);
     }
 
+    private void logIn(){
+
+        mProgrssDialog.setTitle("Log In");
+        mProgrssDialog.setMessage("Please wait....");
+        mProgrssDialog.show();
+
+        final String username = username_et.getText().toString();
+        final String password = password_et.getText().toString();
+
+        if(TextUtils.isEmpty(username)){
+            username_et.setError("Please fill in this field");
+            username_et.requestFocus();
+            mProgrssDialog.dismiss();
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)){
+            password_et.setError("Please fill in this field");
+            password_et.requestFocus();
+            mProgrssDialog.dismiss();
+            return;
+        }
+
+        finish();
+        Intent signUpIntent = new Intent(this,MainMenu.class);
+        startActivity(signUpIntent);
+    }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        callbackManager.onActivityResult(requestCode, resultCode, data);
-        super.onActivityResult(requestCode, resultCode, data);
+    protected void onResume() {
+        super.onResume();
+
+        if (mAnimationDrawable != null && !mAnimationDrawable.isRunning()) {
+            mAnimationDrawable.start();
+        }
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (mAnimationDrawable != null && mAnimationDrawable.isRunning()) {
+            mAnimationDrawable.stop();
+        }
+    }
+
 }
--- app/src/main/java/com/example/lilialobato/pawstragram/ActivitySignup.java
+++ app/src/main/java/com/example/lilialobato/pawstragram/ActivitySignup.java
@@ -1,30 +1,115 @@
 package com.example.lilialobato.pawstragram;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class ActivitySignup extends Activity {
+
+    LinearLayout mLoginContainer;
+    AnimationDrawable mAnimationDrawable;
+
+    EditText username_et, password_et, password_confirm_et;
+    ProgressDialog mProgrssDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        //Animation config
+        mLoginContainer = (LinearLayout) findViewById(R.id.login_container);
+        mAnimationDrawable = (AnimationDrawable) mLoginContainer.getBackground();
+        mAnimationDrawable.setEnterFadeDuration(5000);
+        mAnimationDrawable.setExitFadeDuration(2000);
+
+
+        //login design varibales
+        username_et = (EditText) findViewById(R.id.activity_signup_username_edit);
+        password_et = (EditText) findViewById(R.id.activity_signup_password_edit);
+        password_confirm_et = (EditText) findViewById(R.id.activity_signup_password_confirm);
+        mProgrssDialog = new ProgressDialog(this);
+
     }
 
     public void goBack(View v) {
+        finish();
         Intent intent = new Intent(this, ActivityLogin.class);
         this.startActivity(intent);
     }
 
     public void login(View v){
+        register();
+    }
+
+    private void register() {
+
+
+        mProgrssDialog.setTitle("Creating your account");
+        mProgrssDialog.setMessage("Please wait....");
+        mProgrssDialog.show();
+
+        final String username = username_et.getText().toString();
+        final String password = password_et.getText().toString();
+        String password_confirm = password_confirm_et.getText().toString();
+
+        if (TextUtils.isEmpty(username)) {
+            username_et.setError("Please fill in this field");
+            username_et.requestFocus();
+            mProgrssDialog.dismiss();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            password_et.setError("Please fill in this field");
+            password_et.requestFocus();
+            mProgrssDialog.dismiss();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password_confirm)) {
+            password_confirm_et.setError("Please fill in this field");
+            password_confirm_et.requestFocus();
+            mProgrssDialog.dismiss();
+            return;
+        }
+
+        if (!password.equals(password_confirm)) {
+            password_et.setError("Password charachters don't match!");
+            password_et.requestFocus();
+            mProgrssDialog.dismiss();
+            return;
+        }
+
+        finish();
         Intent intent = new Intent(this, MainMenu.class);
         this.startActivity(intent);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (mAnimationDrawable != null && !mAnimationDrawable.isRunning()) {
+            mAnimationDrawable.start();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (mAnimationDrawable != null && mAnimationDrawable.isRunning()) {
+            mAnimationDrawable.stop();
+        }
+    }
 }
