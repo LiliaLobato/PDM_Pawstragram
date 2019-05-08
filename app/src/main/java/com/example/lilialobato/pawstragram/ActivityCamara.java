410ac1f358ad8cbdb8d762c2a031578e1e4ad7c0
Carlo Bruno <carlobruno_fb@hotmail.com>
2019-05-08 09:20:42 -0500

    added resize example

--- app/src/main/java/com/example/lilialobato/pawstragram/ActivityCamara.java
+++ app/src/main/java/com/example/lilialobato/pawstragram/ActivityCamara.java
@@ -1,7 +1,9 @@
 package com.example.lilialobato.pawstragram;
 
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -10,6 +12,7 @@ import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
@@ -17,6 +20,7 @@ import android.widget.Toast;
 
 public class ActivityCamara extends AppCompatActivity {
     private static final int SELECTED_PIC = 1;
+    private static final int PICK_FROM_GALLERY = 2;
     ImageView imageView;
 
     @Override
@@ -27,9 +31,33 @@ public class ActivityCamara extends AppCompatActivity {
     }
 
     public void onClick(View v) {
-        Intent intent = new Intent(Intent.ACTION_PICK,
-                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(intent, SELECTED_PIC);
+        try {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, PICK_FROM_GALLERY);
+            } else {
+                Intent intent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, SELECTED_PIC);
+            }
+        }
+        catch (Exception e){}
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,  String permissions[], int[] grantResults)
+    {
+        switch (requestCode) {
+            case PICK_FROM_GALLERY:
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Intent galleryIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(galleryIntent, PICK_FROM_GALLERY);
+                } else {
+                    //do something like displaying a message that he didn`t allow the app to access gallery and you wont be able to let him select from gallery
+                }
+                break;
+        }
     }
 
     @Override
@@ -48,11 +76,13 @@ public class ActivityCamara extends AppCompatActivity {
                             String filepath = cursor.getString(columnIndex);
                             cursor.close();
                             Bitmap bitmap = BitmapFactory.decodeFile(filepath);
-                            Drawable drawable = new BitmapDrawable(getResources(), bitmap);
-                            imageView.setBackground(drawable);
+                            Bitmap resize = Bitmap.createScaledBitmap(bitmap, 1080, 1080, false);
+                            imageView.setImageBitmap(resize);
                             Toast toast1 = Toast.makeText(getApplicationContext(), "Prueba ASF", Toast.LENGTH_SHORT);
                             toast1.show();
                         }
+
+
                     }
                 }
                 break;
